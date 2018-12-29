dc8924c9575026fc138ac9c52855cb9bb1e28e31
YammerStudio <46210119+YammerStudio@users.noreply.github.com>
2018-12-29 14:12:22 -0800

    Create OddOrEven.py

---
+++ OddOrEven.py
@@ -0,0 +1,9 @@
+#practicepython.org - Odd or Even  
+
+yourNum = int(input("Enter in a number: "))
+
+if(yourNum % 2 == 0):
+    print("Your number is even!")
+else:
+    print("Your number is odd!")
+
