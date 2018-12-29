114c3af988ed97e17c73cd580343338379fd2b28
YammerStudio <46210119+YammerStudio@users.noreply.github.com>
2018-12-29 03:58:05 -0800

    did extras part 1
    task: Add on to the previous program by asking the user for another number and printing out that many copies of the previous message.

--- ex01.py
+++ ex01.py
@@ -6,3 +6,10 @@ age = int(input("Enter in your age: "))
 hunnid = 100 - age
 
 print("Your name is ", name, " and you will be 1 hundred year old in the year ",  str(int(2018 + hunnid)))
+
+
+#extras 1:
+output = int(input("Enter in a number: "))
+for i in range(output):
+    print("Your name is ", name, " and you will be 1 hundred year old in the year ",  str(int(2018 + hunnid)))
+
