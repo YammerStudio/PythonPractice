7c934a3e226c2fdabd8e6d2bb6e590bdd20eef14
YammerStudio <46210119+YammerStudio@users.noreply.github.com>
2018-12-29 14:25:18 -0800

    added part b of extras

--- ex02.py
+++ ex02.py
@@ -9,4 +9,16 @@ if(yourNum % 2 == 0):
 else:
     print("Your number is odd!")
 
+'''extra 2:  Ask the user for two numbers: one number to check (call it num)
+                and one number to divide by (check).
+                 If check divides evenly into num, tell that to the user. If not,
+                 print a different appropriate message.'''
 
+num = int(input("Enter in a number: "))
+
+check = int(input("Enter in another number: "))
+
+if(num % check == 0):
+    print("Both numbers you entered in are kewl numbers. ")
+else:
+    print("you are lame")
