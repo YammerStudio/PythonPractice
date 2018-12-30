82932b43ab05d911298b48a46904ff319e988a21
YammerStudio <46210119+YammerStudio@users.noreply.github.com>
2018-12-30 14:40:50 -0800

    with extra part 1

---
+++ ex05.py
@@ -0,0 +1,12 @@
+#05 - List Overlap 
+#return a list that is common with no duplicates from two arrays of different sizes.
+
+#extra 1: randomly generate two list to test this: 
+
+istOne = [random.choice(range(20)) for i in range(10)]
+listTwo = [random.choice(range(20)) for i in range(12)]
+
+print('List One: ', listOne)
+print('List Two: ', listTwo)
+print('No duplicates: ', list(set(listOne) | set(listTwo)))
+
